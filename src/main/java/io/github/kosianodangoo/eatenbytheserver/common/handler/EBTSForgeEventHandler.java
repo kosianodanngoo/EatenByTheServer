ce3436bef07468ed9933e4c7cd58c91019984895
kosianodanngoo <80571742+kosianodanngoo@users.noreply.github.com>
2026-03-07 20:40:46 +0900

    無敵貫通強化

---
+++ src/main/java/io/github/kosianodangoo/eatenbytheserver/common/handler/EBTSForgeEventHandler.java
@@ -0,0 +1,29 @@
+package io.github.kosianodangoo.eatenbytheserver.common.handler;
+
+import io.github.kosianodangoo.eatenbytheserver.EatenByTheServer;
+import io.github.kosianodangoo.eatenbytheserver.common.init.EBTSDamageTypes;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = EatenByTheServer.MOD_ID)
+public class EBTSForgeEventHandler {
+    @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
+    public static void onLivingDeath(LivingDeathEvent event) {
+        if (event.getSource().is(EBTSDamageTypes.EATING)) {
+            event.setCanceled(false);
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
+    public static void onLivingHurt(LivingHurtEvent event) {
+        if (event.getSource().is(EBTSDamageTypes.EATING)) {
+            event.setCanceled(false);
+            if (event.getAmount() < 0) {
+                event.setAmount(1);
+            }
+        }
+    }
+}
